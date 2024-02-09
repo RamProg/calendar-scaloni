7cd5b7e6c3ab8e58b56d1a77a524b041ca4b7779
RamProg <ramirodlp@gmail.com>
2024-02-09 14:59:50 -0300

    fix prettier format; add scripts for checks

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -6,4 +6,4 @@ export const daysOfTheWeek = [
   'Thursday',
   'Friday',
   'Saturday',
-]
+];
--- client/src/utils/dates.tsx
+++ client/src/utils/dates.tsx
@@ -1,5 +1,5 @@
 export const getDaysInMonth = (month: number, year: number) =>
-  new Date(year, month, 0).getDate()
+  new Date(year, month, 0).getDate();
 
 export const getFirstDayOfTheMonth = (month: number, year: number) =>
-  new Date(year + '-' + month + '-01').getDay()
+  new Date(year + '-' + month + '-01').getDay();
